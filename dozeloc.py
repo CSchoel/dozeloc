59609edd2b8717031f97382a4f0190e888f0b7e1
Christopher Schölzel <christopher.schoelzel@gmx.net>
2021-11-05 15:02:52 +0100

    sets cwd to test directory

--- dozeloc.py
+++ dozeloc.py
@@ -314,7 +314,12 @@ def run_unittest(test_file, solution_file):
     else:
         subenv["PYTHONPATH"] += ":" + (solution_file.parent)
     # using sys.executable ensures that we use same python for internal tests as for main program
-    res = subprocess.run([sys.executable, test_file], env=subenv, timeout=60, capture_output=True)
+    # TODO set cwd
+    res = subprocess.run(
+        [sys.executable, test_file],
+        env=subenv, timeout=60, capture_output=True,
+        cwd=test_file.parent
+    )
     restxt = "" if len(res.stdout) == 0 else "{}\n\n".format(res.stdout.decode("utf-8"))
     restxt += str(res.stderr.decode("utf-8"))
     return (restxt, res.returncode)
